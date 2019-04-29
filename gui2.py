85b0ed47e7e232be832b79c06be69bf836563586
Maxime Bost-Brown <maxime.bost.brown@live.com>
2019-04-29 01:52:11 -0400

    restructure
    -raw data filters are complete
    -need to complete report w/ visuals
    - need to complete clean data filters

--- gui2.py
+++ gui2.py
@@ -1,51 +1,146 @@
-import kivy
-kivy.require('1.9.0')
+#import kivy
+#kivy.require('1.9.0')
 import pandas as pd
+import numpy as np
+import matplotlib as plt
+import wx
+import dfgui # can install this via pip: https://github.com/bluenote10/PandasDataFrameGUI
 
 from kivy.app import App
 from functools import partial
-from kivy.uix.button import Button
+#from kivy.uix.button import Button
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.uix.boxlayout import BoxLayout # use this to position/organize widgets
-from kivy.uix.recycleview.views import RecycleDataViewBehavior
-from kivy.uix.recyclegridlayout import RecycleGridLayout
-from kivy.uix.popup import Popup
-#from kivy.lang import Builder
+#from kivy.uix.recycleview.views import RecycleDataViewBehavior
+#from kivy.uix.recyclegridlayout import RecycleGridLayout
+#from kivy.uix.popup import Popup
+#from helper_functions import *
 
+Builder.load_string("""
+<MenuScreen>:
+    BoxLayout:
+        Button:
+            text: "[b]Generate Report[b]"
+            markup: True
+            font_size: 30 
+            size_hint_x: 0.25
+            size_hint_y: 0.25
+            pos_hint: {'center_y': 0.5}
+            on_press: root.manager.current = 'report'
+        Button:
+            text: "[b]View Data[/b]"
+            markup: True
+            font_size: 30 
+            size_hint_x: 0.25
+            size_hint_y: 0.25
+            pos_hint: {'center_y': 0.5}
+            on_press: root.manager.current = 'viewData'
+        Button:
+            text: "[b]View Raw Data[/b]"
+            markup: True
+            font_size: 30 
+            size_hint_x: 0.25
+            size_hint_y: 0.25
+            pos_hint: {'center_y': 0.5}     
+            #background_color: (155,0,51,53) # comment
+            on_press: root.manager.current = 'rawData'
+<ReportScreen>:
+    BoxLayout:
+        Button:
+            text: "Generate Report"
+        Button:
+            text: "main menu"
+            on_press: root.manager.current = 'menu'
+<ViewDataScreen>:
+    BoxLayout:
+        Button:
+            text: "view data"
+        Button:
+            text: "main menu"
+            on_press: root.manager.current = 'menu'
+<RawDataScreen>:
+    BoxLayout:
+        Button:
+            text: "view raw data"
+            on_press: root.manager.current = "rawDataFrame"
+        Button:
+            text: "main menu"
+            on_press: root.manager.current = 'menu'
+<RawDataFrameScreen>:
+    BoxLayout:
+        Button:
+            text: "raw clinton data"
+            on_press: root.clintonRawFrame()
+        Button:
+            text: "raw trump data"
+            on_press: root.trumpRawFrame()
+        Button:
+            text: "filter congress data"
+        Button:
+            text: "main menu"
+            on_press: root.manager.current = 'menu'
 
-class ReportButton(Button):
+        
+""")
+
+# data
+clintonRaw = pd.read_csv('clinton_raw.csv')
+
+# Screens
+class MenuScreen(Screen):
     pass
 
-class ViewDataButton(Button):
+class ReportScreen(Screen):
     pass
 
-class FilterDataButton(Button):
-    pass   
+class ViewDataScreen(Screen):
+    pass
+
+class RawDataScreen(Screen):
+    pass
+
+class RawDataFrameScreen(Screen):
+    
+    def clintonRawFrame(self):
+        clintonRaw = pd.read_csv('clinton_raw.csv')
+        dfgui.show(clintonRaw)
+    
+    def trumpRawFrame(self):
+        trumpRaw = pd.read_csv('trump_raw.csv')
+        dfgui.show(trumpRaw)
+
+# Screen Manager
+sm = ScreenManager()
+sm.add_widget(MenuScreen(name='menu'))
+sm.add_widget(ReportScreen(name='report'))
+sm.add_widget(ViewDataScreen(name='viewData'))
+sm.add_widget(RawDataScreen(name='rawData'))
+sm.add_widget(RawDataFrameScreen(name='rawDataFrame'))
 
 
-class Gui2App(BoxLayout, App):
+
+class Gui2App(App):
 
     # disable the button when pressed
-    def disable(self, instance, *args):
-        instance.disabled = True
+    #def disable(self, instance, *args):
+    #    instance.disabled = True
 
     # change the text of  the button when pressed
-    def update(self, instance, *args):
-        instance.text = "Disabled Button!"
+    #def update(self, instance, *args):
+    #    instance.text = "Disabled Button!"
 
-    def get_data(self):
-        df = pd.read_excel("clinton_raw.csv")
-        
+    #def get_data(self):
+    #    df = pd.read_excel("clinton_raw.csv")    
     def build(self):
         # mybtn = Button(text="Click me to disable", background_color = (155,0,51,53), pos = (300, 500), size_hint = (.25, .18))
         # mybtn.bind(on_press=partial(self.disable, mybtn))
         # mybtn.bind(on_press=partial(self.update, mybtn))
         #return Label(text = "[color=99ffcc][u][b]this[/b] label[/u][/color] is the [i]best[/i] label [color=f50000][b]known to man[/b][/color]", markup = True, font_size = '30')
-        self.add_widget(ReportButton())
-        self.add_widget(ViewDataButton())
-        self.add_widget(FilterDataButton())
-        return self
+        #self.add_widget(ReportButton())
+        #self.add_widget(ViewDataButton())
+        #self.add_widget(FilterDataButton())
+        return sm
 
-
-gui2 = Gui2App()
-
-gui2.run()
+if __name__ == '__main__':
+    Gui2App().run()
---
+++ helper_functions.py
@@ -0,0 +1,422 @@
+#%% [markdown]
+# All of the helper commands for filtering datasets to send just what you want to a visualization tool.
+# 
+# Designed so you send a dataset through a series of helper function filters, then send the final filtered dataset to a function that provides an answer
+# 
+# 20-April-2019
+# 
+# 21-April: Updated function names to be more intuitive/conventional
+
+#%%
+
+
+#%% [markdown]
+# List of Helpers:
+# 
+# Filters
+# 
+# filter_age      Gives a dataset only including tweets by people above or below (or exactly) an age
+# 
+# filter_sentiment_type   Gives a dataset with only positive, negative, or neutral tweets
+# 
+# filter_sentiment_number Gives a dataset with sentiment valusea abve or below a number (find more extreme sentiment)
+# 
+# filter_gender   Gives a dataset with only tweets from specified gender
+# 
+# filter_leaning_tweets  Gives a dataset only from liberal or conservative
+# 
+# filter_persons_tweets  Gives dataset with only one person's tweets
+# 
+# filter_race_type   Gives a dataset containing only the race entered
+# 
+# 
+# Answers
+# 
+# get_sentiment_count   Returns the count of tweets containing that classification of sentiment (+ 0 -)
+# 
+# get_total_tweet_count  Returns number of rows in dataset
+# 
+# get_retweets_per_tweet  Returns sum of retweet_count divided by number of tweets
+# 
+# get_average_sentiment  Returns the average sentiment score in a dataset
+# 
+# get_leaning_score Returns the political leaning score of a person
+# 
+# get_sentiment_frequency  returns proportion of tweets that are a particular type of sentiment
+# 
+# get_top_retweeted  returns a ranked dictionary of the most retweeted by their retweets_per_tweet and their retweet rates
+# 
+# get_most_conservative  returns a dictionary of the most conservative by dw_score and their scores
+# 
+# get_most liberal  Same as above but for liberal
+# 
+# get_most_positive  returns ranked dictionary of names and average sentiment of most positive average sentiment
+# 
+# get_most_negative  same as above for negative
+# 
+# 
+
+#%%
+import csv
+import pandas as pd
+import numpy as np
+
+
+#%%
+trump=pd.read_csv('trump_sentiment.csv')
+clinton=pd.read_csv('clinton_sentiment.csv')
+congress=pd.read_csv('congress_sentiment.csv')
+all_names=congress['elite'].unique()
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+congress.head()
+
+
+#%%
+##Valid arguments for side are "more than", "less than", "exactly"
+def filter_age(dataset, age, side): 
+    dataset['age']=pd.to_numeric(dataset['age'])
+    if(side=="more than"):
+        new_data=dataset['age']>age
+    elif(side=="less than"):
+        new_data=dataset['age']<age
+    elif(side=="exactly"):
+        new_data=dataset['age']==age
+    else:
+        print("age direction not given, ignoring age filter")
+        return dataset   
+    
+    return dataset[new_data]
+
+
+#%%
+x=filter_age(congress,64, side="d")
+x.head()
+
+
+#%%
+###direction takes "above", "below", "exactly"
+def filter_sentiment_number(dataset, number, direction):
+    if (number>1 or number<-1):
+        print("\n"+"invalid number range, retruning entire dataset")
+        return dataset
+    
+    if (direction=="above"):
+        new_data=dataset['Vader_Score']>number
+    elif(direction=="below"):
+        new_data=dataset['Vader_Score']<number
+    elif(direction=="exactly"):
+        new_data=dataset['Vader_Score']==number
+    else:
+        print ("invalid direction given, returning entire dataset")
+        return dataset
+    
+    
+    return dataset[new_data]
+
+
+#%%
+filter_sentiment_number(congress, 10, 'below')
+
+
+#%%
+##pnn takes "positive,", "negative", "neutral".  
+def filter_sentiment_type(dataset, pnn):
+    if (pnn=='positive'):
+        new_data=dataset['Trinary_Score']==1
+    elif (pnn=='negative'):
+        new_data=dataset['Trinary_Score']==-1
+    elif (pnn=='neutral'):
+        new_data=dataset['Trinary_Score']==0
+    else:
+        print("invalid type given, returning entire dataset")
+        return dataset
+    
+    return dataset[new_data]
+    
+
+
+#%%
+##pnn takes "positive,", "negative", "neutral".  
+def get_sentiment_count(dataset, pnn):
+    num=get_sentiment_type(dataset,pnn).shape[0]
+    return num
+
+
+#%%
+##Gender takes "male or "female"
+def filter_gender(dataset, gender):
+    if(gender==male):
+        new_data=dataset['gender']==0.5
+    elif(gender=="female"):
+        new_data=dataset['gender']==-0.5
+    else:
+        print("invalid gender given, returning entire dataset")
+        return dataset
+    
+    return dataset[new_data]
+
+
+#%%
+##leans takes liberal or conservative
+def filter_leaning_tweets(dataset, leans):
+    if(leans=="conservative"):
+        new_data=dataset['dw_score']>0
+    elif(leans=="liberal"):
+        new_data=dataset['dw_score']<0
+    else:
+        print("invalid leaning given, returning entire dataset")
+        return dataset
+    
+    return dataset[new_data]
+
+
+#%%
+def get_total_tweet_count(dataset):
+    total, ignore =dataset.shape
+    return total
+
+
+#%%
+def get_persons_tweet_count(dataset, name):
+    pertinent=dataset['elite']==name
+    return get_total_tweet_count(dataset[pertinent])
+
+
+#%%
+
+
+
+#%%
+def filter_persons_tweets(dataset,name):
+    pertinent=dataset['elite']==name
+    return dataset[pertinent]
+
+
+#%%
+def get_total_retweet_count(dataset):
+    the_sum=dataset['retweet_count'].sum()
+    return the_sum
+
+
+#%%
+
+
+
+#%%
+def get_retweets_per_tweet(dataset):
+    rt=get_total_retweet_count(dataset)
+    tw=get_total_tweet_count(dataset)
+    ans=rt/tw
+    return ans
+
+
+#%%
+def get_top_retweeted(dataset, num):
+    rts=[]
+    ranks={}
+    final={}
+    all_names=dataset['elite'].unique()
+    for name in all_names:
+        w=filter_persons_tweets(dataset,name)
+        x=get_retweets_per_tweet(w)
+        rts.append(x)
+    for d,c in zip(all_names,rts):
+        ranks.update({d:c})
+    ranks_sorted=sorted(ranks, key=ranks.get, reverse=True)
+    for name in ranks_sorted[0:num]:
+        final.update({name:ranks[name]})
+    return final
+    
+
+
+#%%
+def get_average_sentiment(dataset):
+    num=dataset.shape[0]
+    sent=dataset['Vader_Score'].sum()
+    return sent/num
+
+
+#%%
+get_average_sentiment(filter_persons_tweets(congress, "_Hunter"))
+
+
+#%%
+get_top_retweeted(congress,5)
+
+
+#%%
+def get_leaning_score(dataset,name):
+    score=filter_persons_tweets(dataset,name)['dw_score'].max()
+    return score
+
+
+#%%
+
+
+
+#%%
+get_leaning_score(congress,"_Hunter")
+
+
+#%%
+def get_most_conservative(dataset,num):
+    rts=[]
+    ranks={}
+    final={}
+    all_names=dataset['elite'].unique()
+    for name in all_names:
+        score=get_leaning_score(dataset,name)
+        rts.append(score)
+    for d,c in zip(all_names,rts):
+        ranks.update({d:c})
+    ranks_sorted=sorted(ranks, key=ranks.get, reverse=True)
+    for name in ranks_sorted[0:num]:
+        final.update({name:ranks[name]})
+    return final
+
+
+#%%
+get_most_conservative(congress,5)
+
+
+#%%
+def get_most_liberal(dataset,num):
+    rts=[]
+    ranks={}
+    final={}
+    all_names=dataset['elite'].unique()
+    for name in all_names:
+        score=get_leaning_score(dataset,name)
+        rts.append(score)
+    for d,c in zip(all_names,rts):
+        ranks.update({d:c})
+    ranks_sorted=sorted(ranks, key=ranks.get, reverse=False)
+    for name in ranks_sorted[0:num]:
+        final.update({name:ranks[name]})
+    return final
+
+
+#%%
+get_most_liberal(congress,6)
+
+
+#%%
+##Race is "white", or "not white"
+def filter_race_type(dataset, race):
+    if(race=="white"):
+        new_data=dataset['race']==0.5
+    elif(race=="not white"):
+        new_data=dataset['race']==-0.5
+    else:
+        print("invalid race entered, returning entire dataset")
+        return dataset
+    return dataset[new_data]
+
+
+#%%
+##pnn takes "positive,", "negative", "neutral".  
+def get_sentiment_frequency(dataset,pnn):
+    count=filter_sentiment_type(dataset,pnn).shape[0]
+    tot=dataset.shape[0]
+    return count/tot
+    
+
+
+#%%
+get_sentiment_frequency(trump,'negative')
+
+
+#%%
+def get_most_positive(dataset,num):
+    rts=[]
+    ranks={}
+    final={}
+    all_names=dataset['elite'].unique()
+    for name in all_names:
+        dataset2=filter_persons_tweets(dataset,name)
+        score=get_average_sentiment(dataset2)
+        rts.append(score)
+    for d,c in zip(all_names,rts):
+        ranks.update({d:c})
+    ranks_sorted=sorted(ranks, key=ranks.get, reverse=True)
+    for name in ranks_sorted[0:num]:
+        final.update({name:ranks[name]})
+    return final
+
+
+#%%
+get_most_positive(congress,10)
+
+
+#%%
+def get_most_negative(dataset,num):
+    rts=[]
+    ranks={}
+    final={}
+    all_names=dataset['elite'].unique()
+    for name in all_names:
+        dataset2=filter_persons_tweets(dataset,name)
+        score=get_average_sentiment(dataset2)
+        rts.append(score)
+    for d,c in zip(all_names,rts):
+        ranks.update({d:c})
+    ranks_sorted=sorted(ranks, key=ranks.get, reverse=False)
+    for name in ranks_sorted[0:num]:
+        final.update({name:ranks[name]})
+    return final
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
+#%%
+
+
+
